61b3b0301ffc228a2b0fcae32352e9ab36c5fa3d
LiamDrew <liamdrew92@icloud.com>
2025-01-15 20:32:16 -0500

    removing stuff

--- experiments/PerformantUM/Profiling/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
-    asm volatile("marker_label2: .inst 0xCAFEBABE");
-    return 0;
-}
--- experiments/PerformantUM/run.sh
+++
@@ -1,4 +0,0 @@
-make clean > /dev/null
-make > /dev/null
-
-/usr/bin/time ./um sandmark.umz
